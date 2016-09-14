dd7fb452817c8a25178b541b0cf7d48e77f88597
cjjava <85309651@qq.com>
2016-09-14 10:46:43 +0800

    添加对jquery.validationEngine的集成例子

--- src/angular-ad-validate.js
+++ src/angular-ad-validate.js
@@ -2,7 +2,7 @@
 	'use strict';
 	$.validationEngineLanguage.allRules['ad-required']={
 		"func": function(field){
-			return (field.val() === "object:5") ? false : true;
+			return (field.find("option:selected").text() === "请选择") ? false : true;
 		},
 		"alertText": "* 此处不可空白"
 	};
